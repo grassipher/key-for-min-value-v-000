49259a95794aca34029c52ff0b995da7553febaf
Gabriel Valle <gvalle91@gmail.com>
2018-06-15 20:29:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value = 0
-  min_value_key = ""
+  min_value_key = nil
   name_hash.collect do |key, value|
     if min_value != value
       if value < min_value
