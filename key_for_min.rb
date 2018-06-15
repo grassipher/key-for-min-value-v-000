1ad9519b7fc3fd12b2f48a92eebede20ae192dd8
Gabriel Valle <gvalle91@gmail.com>
2018-06-15 20:30:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,16 +3,20 @@
 
 def key_for_min_value(name_hash)
   min_value = 0
-  min_value_key = nil
-  name_hash.collect do |key, value|
-    if min_value != value
-      if value < min_value
+  if name_hash.size == 0
+    min_value_key = nil
+  else
+    min_value_key = ""
+    name_hash.collect do |key, value|
+      if min_value != value
+        if value < min_value
+          min_value = value
+          min_value_key = key
+        end
+      else
         min_value = value
         min_value_key = key
       end
-    else
-      min_value = value
-      min_value_key = key
     end
   end
   return min_value_key
